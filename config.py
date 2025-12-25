067b5dbb843962aa4150854fb7b365ef3c97b4c9
JaniRPLover <fuyxycivivucyfu143@gmail.com>
2025-12-25 10:08:24 +0530

    Update upstream repository URL in config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Jani-Music/HemaRP",
+    "https://github.com/JaniRPLover/Radhya",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
